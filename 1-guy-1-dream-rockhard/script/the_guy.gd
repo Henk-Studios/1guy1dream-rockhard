616d64191fd0cad7bfd23b9bbf24a3246310ef01
KwantumQ <99174287+KwantumQ@users.noreply.github.com>
2026-04-23 20:37:57 +0200

    guuy

--- 1-guy-1-dream-rockhard/script/the_guy.gd
+++ 1-guy-1-dream-rockhard/script/the_guy.gd
@@ -1,33 +1,28 @@
 extends RigidBody2D
 
-@export var thrust_force := 600.0
-@export var torque_force := 1200.0
-@export var max_angular_velocity := 4.0
-
-@export var stabilize_force := 200.0
-
+@export var thrust_force := 900.0
+@export var torque_force := 900.0
+@export var max_angular_velocity := 5.0
 
 
 func _ready():
-	self.linear_damp = linear_damp
-	self.angular_damp = angular_damp
+	self.linear_damp = 1.5
+	self.angular_damp = 2.0
 
 func _physics_process(delta):
-	var thrust_dir = -transform.y  # local "up"
-
-	# --- STABILIZING LIFT ---
-	var upright_factor = clamp(Vector2.UP.dot(thrust_dir), 0.0, 1.0)
-	apply_force(thrust_dir * stabilize_force * upright_factor)
+	# Define fixed thrust directions
+	var left_dir = Vector2(1, -1.5).normalized()   # up-left
+	var right_dir = Vector2(-1, -1.5).normalized()   # up-right
 
 	# LEFT JET
-	if Input.is_action_pressed("rightjet"):
-		apply_force(thrust_dir * thrust_force)
+	if Input.is_action_pressed("leftjet"):
+		apply_force(left_dir * thrust_force)
 		apply_torque(-torque_force)
 
 	# RIGHT JET
-	if Input.is_action_pressed("leftjet"):
-		apply_force(thrust_dir * thrust_force)
+	if Input.is_action_pressed("rightjet"):
+		apply_force(right_dir * thrust_force)
 		apply_torque(torque_force)
 
-	# Clamp spin so it doesn't go crazy
+	# Clamp rotation speed
 	angular_velocity = clamp(angular_velocity, -max_angular_velocity, max_angular_velocity)
