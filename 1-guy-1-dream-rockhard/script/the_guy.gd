d43638089d2ad3f6568dd12ae4f0eb5d7a8d12a0
KwantumQ <99174287+KwantumQ@users.noreply.github.com>
2026-04-24 00:03:06 +0200

    Revert "Merge branch 'main' into 1guy"
    This reverts commit c67e89659a10865c55549e5daeec1aa045383a7e, reversing
    changes made to 4ef1db6fffe3952c5a75ffa11924f91c43fbbcb2.

--- 1-guy-1-dream-rockhard/script/the_guy.gd
+++ 1-guy-1-dream-rockhard/script/the_guy.gd
@@ -3,7 +3,7 @@ extends RigidBody2D
 @export var thrust_force := 900.0
 @export var torque_force := 900.0
 @export var max_angular_velocity := 5.0
-@export var audio_player: Node
+
 
 func _ready():
 	self.linear_damp = 1.5
@@ -23,18 +23,20 @@ func _physics_process(delta):
 	var left_dir = Vector2(1, -1.5).normalized() # up-left
 	var right_dir = Vector2(-1, -1.5).normalized() # up-right
 
-	var jetting = false
 	# LEFT JET
 	if Input.is_action_pressed("leftjet"):
 		apply_force(left_dir * Global.jetpackspeed)
 		apply_torque(-torque_force)
-		audio_player.start_looping_sfx("jetfart")
-		jetting = true
 
 	# RIGHT JET
 	if Input.is_action_pressed("rightjet"):
 		apply_force(right_dir * Global.jetpackspeed)
 		apply_torque(torque_force)
-
+		
+	if Input.is_action_pressed("rightjet") or Input.is_action_pressed("leftjet"):
+		$jetpart.emitting = true
+	else:
+		$jetpart.emitting = false
+		
 	# Clamp rotation speed
 	angular_velocity = clamp(angular_velocity, -max_angular_velocity, max_angular_velocity)
