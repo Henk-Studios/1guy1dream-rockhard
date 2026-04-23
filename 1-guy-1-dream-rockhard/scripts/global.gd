3c3008e69b03cad640ce2e25c6ccffa6072f6226
Jeroen Krijgsman <jeroenkrijgsman@outlook.com>
2026-04-24 00:38:03 +0200

    rebalance

--- 1-guy-1-dream-rockhard/scripts/global.gd
+++ 1-guy-1-dream-rockhard/scripts/global.gd
@@ -9,7 +9,7 @@ var jetpackspeed = 700
 var money = 0
 var damage = 1
 var width := 0.1 # cone half-angle (radians)
-var particles_per_second := 3
+var particles_per_second := 5
 var particle_speed := 300.0
 
 # Explosive-bullet upgrades. 0 = no explosions.
